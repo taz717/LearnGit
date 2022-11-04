d4e014142a7edfd1d73328c09e97d2fa130ae427
Moataz Khallaf <MoatazKhallaf@outlook.com>
2022-11-04 10:36:48 -0600

    Add python file for git help

---
+++ helloWord.py
@@ -0,0 +1,22 @@
+# Moataz Khallaf (Tazatronator) 2022-11-04
+# Git Help using python file as test file
+
+students = [
+    "Taz",
+]
+
+
+def hello_students(students):
+    # This function will print hello to all students in the list
+
+    for student in students:
+        print(f"Hello {student}!")
+
+    return
+
+
+if __name__ == "__main__":
+    # This is the main function
+    hello_students(students)
+
+# End of File
